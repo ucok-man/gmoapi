3717ce8bad3d8f38abe5f68c30c3f418de388a1d
Alvin Odhi N <ucokkocu411@gmail.com>
2025-08-26 10:05:50 +0700

    feat: init + setup project

---
+++ cmd/api/handler_movie.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "show the details of movie %d\n", id)
+}
+
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "create movie \n")
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ucok-man/gmoapi
+
+go 1.25.0
+
+require github.com/julienschmidt/httprouter v1.3.0 // indirect

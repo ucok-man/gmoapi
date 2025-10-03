31f673f27a5e9e5bf8d467ed44764f9f487f21b1
Alvin Odhi N <ucokkocu411@gmail.com>
2025-10-03 23:35:20 +0700

    chore: add nixpacks and include citext on migration

---
+++ migrations/20250826074526_add_citext_extension.sql
@@ -0,0 +1,10 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE EXTENSION IF NOT EXISTS citext;
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP EXTENSION IF EXISTS citext;
+
+-- +goose StatementEnd
